86a22b102953c3d04dadaef65a28afb5ee49cf42
Russell Simpkins <russellsimpkins@gmail.com>
2015-03-17 14:57:37 -0400

    test the user functions

---
+++ user_test.go
@@ -0,0 +1,22 @@
+package clip
+
+import (
+	"testing"
+)
+
+func TestAddUser(t *testing.T) {
+
+	var user User
+	user = User{}
+	user.First = "Russ"
+	user.Last = "Simpkins"
+	user.Email = "russell.simpkins@nytimes.com"
+
+	err := AddUser(&user)
+	if err != nil {
+		t.Log("There was an error: ", err)
+		t.Fail()
+	}
+
+	
+}
