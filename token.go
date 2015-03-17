e51c4e432d94e30919410f953d0486b4c19e46f1
Eric Schorr <eschorr@nytimes.com>
2015-03-17 16:18:11 +0000

    fix token.go

--- token.go
+++ token.go
@@ -1,9 +1,42 @@
 package clip
 
-func AddToken(token Token) (err error, token Token) {
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func randInt(min int, max int) int {
+    return min + rand.Intn(max-min)
+}
+
+func AddToken(token Token) (err error, token1 Token) {
+	return
 }
 
 func DeleteToken(token Token) (err error) {
-
+	return
 }
+
+func GenerateToken() (token Token) {
+
+	rndString := make([]byte, 10)
+    for i:= 0 ; i < 10 ; i++ {
+        rndString[i] = byte(randInt(65,90))
+    }
+
+	hash := sha256.New()
+	hash.Write(rndString)
+	md := hash.Sum(nil)
+	Token.StringValue = hex.EncodeToString(md)
+	Token.IntValue = crc32.ChecksumIEEE(md)
+    fmt.Printf("This is my value %d\n", Token.IntValue)
+    return
+}
+
--- user.go
+++ user.go
@@ -7,9 +7,9 @@ import (
 
 // this file holds all the logic for handling users in the system
 type ClipUser interface {
-	func AddUser(user User) (err error, user User)
-	func UpdateUser(user User) (err error)
-	func DeleteUser(user User) (err error)
+	AddUser(user User) (err error, user User)
+	UpdateUser(user User) (err error)
+	DeleteUser(user User) (err error)
 }
 
 DoSomething() {
@@ -32,12 +32,12 @@ func getNextUserId() (err error, id uint32) {
 	if err != nil {
 		return
 	}
-	
-	_, err = redis.Scan(reply, &id)	
+
+	_, err = redis.Scan(reply, &id)
 }
 
 func (u *ClipUser) AddUser(user User) (err error, user User) {
-	
+
 }
 
 func (u *ClipUser) UpdateUser(user User) (err error) {
