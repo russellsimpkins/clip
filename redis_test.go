008c00e967a88caea7b1cb9ac7a7d2a2b2c909a2
Russell Simpkins <russellsimpkins@gmail.com>
2015-03-17 15:15:07 -0400

    making more tests

--- redis_test.go
+++ redis_test.go
@@ -1,10 +1,33 @@
 package clip
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestPutRedis(t *testing.T) {
-	fmt.Print("Im in it to win it")
+	var (
+		r   RedisHelper
+		id  int64
+		err error
+	)
+	r = NewRedisHelper()
+	defer r.Close()
+
+	id, err = r.GetNextVal("sys.Token")
+	if err != nil {
+		t.Log("Something's wrong. We didn't get back a value", err)
+		t.Fail()
+		return
+	}
+	t.Log("We got a value %d", id)
+	id, err = r.GetNextVal("test.Token")
+	if err != nil {
+		t.Log("Something's wrong. We didn't get back a value", err)
+		t.Fail()
+		return
+	}
+	// cleaning up after myself
+	_, _ = r.Conn.Do("DEL", "sys.Token")
+	t.Log("We got a value %d", id)
+
 }
