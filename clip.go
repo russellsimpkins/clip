d3057fabcbbb9736321f47357f72a9dd6e6fa762
Russell Simpkins <russellsimpkins@gmail.com>
2015-03-17 22:12:39 -0400

    added a general senderror func here

--- clip.go
+++ clip.go
@@ -1,2 +1,18 @@
 package clip
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func SendError(status int, data string, writer http.ResponseWriter) {
+	what := WebResponse{}
+	what.Status = status
+	what.Message = data
+	body, _ := json.Marshal(what)
+	out := string(body)
+	log.Print(out)
+	http.Error(writer, out, status)
+	return
+}
