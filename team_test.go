87d96ce2e571addf634630a9ad1cb3b77c0de1c8
Russell Simpkins <russellsimpkins@gmail.com>
2015-03-19 11:27:04 -0400

    remove routes

--- team_test.go
+++ team_test.go
@@ -10,21 +10,6 @@ import (
 	"testing"
 )
 
-func SetRoutes(router *mux.Router) {
-
-	router.HandleFunc("/svc/clip/teams", GetTeamsHandler).Methods("GET")
-	router.HandleFunc("/svc/clip/team", CreateTeamHandler).Methods("POST")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\.\\-_]+}", UpdateTeamHandler).Methods("PUT")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}", GetTeamHandler).Methods("GET")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}", DeleteTeamHandler).Methods("DELETE")
-	// token routes
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\.\\-_]+}/token", CreateTokenHandler).Methods("POST")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\.\\-_]+}/token/{token:[a-zA-Z0-9]+}", UpdateTokenHandler).Methods("PUT")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}", GetTokenHandler).Methods("GET")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}", DeleteTokenHandler).Methods("DELETE")
-	return
-}
-
 func TeamNames() (teams []string) {
 	teams = make([]string, 5)
 	teams[0] = "Data Universe"
