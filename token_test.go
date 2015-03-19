1a957ea437c43bc7f2870de17e1dde2dacc03468
Russell Simpkins <russellsimpkins@gmail.com>
2015-03-19 11:22:51 -0400

    got the test working

--- token_test.go
+++ token_test.go
@@ -10,29 +10,13 @@ import (
 	"testing"
 )
 
-func setRoutes(router *mux.Router) {
-
-	router.HandleFunc("/svc/clip/teams", GetTeamsHandler).Methods("GET")
-	router.HandleFunc("/svc/clip/team", CreateTeamHandler).Methods("POST")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\.\\-_]+}", UpdateTeamHandler).Methods("PUT")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}", GetTeamHandler).Methods("GET")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}", DeleteTeamHandler).Methods("DELETE")
-	// token routes
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\.\\-_]+}/token", CreateTokenHandler).Methods("POST")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\.\\-_]+}/token/{token:[a-zA-Z0-9]+}", UpdateTokenHandler).Methods("PUT")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}", GetTokenHandler).Methods("GET")
-	router.HandleFunc("/svc/clip/team/{name:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}", DeleteTokenHandler).Methods("DELETE")
-	return
-}
-
-
 func TestToken(t *testing.T) {
 	token := GenerateToken()
 	fmt.Printf("Token.IntValue    = %d\n", token.IntValue)
 	fmt.Printf("Token.StringValue = %s\n", token.StringValue)
 }
 
-func setupTeam() (team Team) {
+func setupTeam(team *Team) {
 
 	var (
 		router *mux.Router
@@ -40,13 +24,15 @@ func setupTeam() (team Team) {
 	)
 	
 	router = mux.NewRouter()
-	setRoutes(router)
-	team = Team{}
+	SetRoutes(router)
+
 	team.Name = "Test"
 	data, _ := json.Marshal(team)
+	//fmt.Println("DATA: ", string(data))
 	request, _ = http.NewRequest("POST", "/svc/clip/team", strings.NewReader(string(data)))
 	response := httptest.NewRecorder()
 	router.ServeHTTP(response, request)
+	//fmt.Println(response.Body.String())
 	return
 }
 
@@ -62,33 +48,54 @@ func TestTokenCrud(t *testing.T) {
 	SetRoutes(router)
 	
 	// Create a team
-	team := setupTeam()
-		
-	w := httptest.NewRecorder()	
-	request, _ = http.NewRequest("POST", "/svc/clip/team/DU/token", nil)
+	team := Team{}
+	team.Name = "Test"
+	setupTeam(&team)
+	t.Log("INFO: team: ", team)
+
+	w := httptest.NewRecorder()
+	url := fmt.Sprintf("/svc/clip/team/%s/token", team.Name)
+	//t.Log("INFO: url: ", url)
+	request, _ = http.NewRequest("POST", url, nil)
+	
 	router.ServeHTTP(w, request)
 	t.Log(w.Body.String())
+	
 	if w.Code != 200 {
 		t.Log("ERROR: ", w.Body.String())
 		t.Fail()
 	}
 	
-	_ = json.Unmarshal([]byte(w.Body.String()), &team)
-	
-	//team.Token[0].Applications = make(map[string]Feature, 2)
-	//f := Feature{}
-	//f.Flags = make(map[string]Flag, 2)
-	//fl := f.Flags["chocolate"]
-	//fl.Sandbox = 1
-	//fl.Development = 1
-	//fl.Attribs = make(map[string]int, 1)
-	//fl.Attribs["displayFE"] = 1
-	//f.Flags["chocolate"] = fl
-	//team.Token[0].Applications["doughnuts"] = f
+	url = fmt.Sprintf("/svc/clip/team/%s", team.Name)
+	request, _ = http.NewRequest("GET", url, nil)
+	w = httptest.NewRecorder()
+	router.ServeHTTP(w, request)
+	team = Team{}
+	err := json.Unmarshal(w.Body.Bytes(), &team)
+
+	if err != nil {
+		return
+	}
+	team.Token[0].Applications = make(map[string]Feature, 2)
+	f := Feature{}
+	f.Flags = make(map[string]Flag, 2)
+	fl := f.Flags["chocolate"]
+	fl.Sandbox = 1
+	fl.Development = 1
+	fl.Attribs = make(map[string]int, 1)
+	fl.Attribs["displayFE"] = 1
+	f.Flags["chocolate"] = fl
+	team.Token[0].Applications["doughnuts"] = f
 	
 	
 	data, _ := json.Marshal(team)
+	
 	t.Log(string(data))
+
+	url = fmt.Sprintf("/svc/clip/team/%s", team.Name)
+	request, _ = http.NewRequest("PUT", url, strings.NewReader(string(data)))
+	router.ServeHTTP(w, request)
+	t.Log(w.Body.String())
 	cleanUp(&team)
 }
 
@@ -98,7 +105,6 @@ func cleanUp(team *Team) {
 	router = mux.NewRouter()
 	SetRoutes(router)
 	response := httptest.NewRecorder()
-
 	request, _ := http.NewRequest("DELETE", fmt.Sprintf("/svc/clip/team/%s", team.Name), nil)
 	router.ServeHTTP(response, request)
 	
