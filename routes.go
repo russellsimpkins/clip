ff1d27c08710072e1ad5f7d4cd07fc09c559ec84
Russell Simpkins <russellsimpkins@gmail.com>
2015-03-19 22:05:05 -0400

    added down to env pick

--- routes.go
+++ routes.go
@@ -19,10 +19,20 @@ func SetRoutes(router *mux.Router) {
 	router.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\.\\-_]+}/token",
 		CreateTokenHandler).Methods("POST")
 	router.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
-		UpdateTokenHandler).Methods("PUT")
+		UpdateTokenHandler).Methods("PUT")	
+	router.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
+		DeleteTokenHandler).Methods("DELETE")
 	router.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
 		GetTokenHandler).Methods("GET")
-	router.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}",
-	DeleteTokenHandler).Methods("DELETE")
+	router.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}/{app:[a-zA-Z0-9 \\%\\.\\-_]+}",
+		GetTokenAppHandler).Methods("GET")
+	router.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}/{app:[a-zA-Z0-9 \\%\\.\\-_]+}/{feature:[a-zA-Z0-9 \\%\\.\\-_]+}",
+		GetTokenAppFeatureHandler).Methods("GET")
+	router.HandleFunc("/svc/clip/team/{team:[a-zA-Z0-9 \\%\\.\\-_]+}/token/{token:[a-zA-Z0-9]+}/{app:[a-zA-Z0-9 \\%\\.\\-_]+}/{feature:[a-zA-Z0-9 \\%\\.\\-_]+}/{env:(sbx|dev|int|stg|prd)}",
+		GetTokenAppFeatureEnvHandler).Methods("GET")
+
+	
+
+	
 
 }
